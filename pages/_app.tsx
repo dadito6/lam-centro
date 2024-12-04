198bb2203b00102d1ce6b3c3a7be4be290563594
cesar rea <cesararielrea9@gmail.com>
2024-12-04 19:27:03 -0300

    Nuevo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import { AppProps } from 'next/app';
-
+import '../styles/globals.css'; // Asegúrate de que esta línea esté presente
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
