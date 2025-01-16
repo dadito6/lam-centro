7b434f2200de050051d7ffbc2cae201017e087b8
cesar rea <cesararielrea9@gmail.com>
2025-01-15 22:56:01 -0300

    agrego head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,10 @@ import '../globals.css'; // Asegúrate de importar el archivo de estilos
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div className="min-h-screen">
+     <Head>
+  <title>    LAM </title>
+  <link rel="icon" href="/lam.png" />
+</Head>
       <div className="background animated-gradient"> {/* Fondo animado */}
         <Component {...pageProps} />
       </div>
