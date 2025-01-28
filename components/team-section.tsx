f3979bbc6646c756b8816a5956e2d36559223bbe
cesar rea <cesararielrea9@gmail.com>
2025-01-28 15:55:34 -0300

    cambio Carrusel

--- components/team-section.tsx
+++ components/team-section.tsx
@@ -58,8 +58,8 @@ export function TeamSection() {
     },
     mobile: {
       breakpoint: { max: 464, min: 0 },
-      items: 1,
-      slidesToSlide: 1 // optional, default to 1.
+      items: 2,
+      slidesToSlide: 2 // optional, default to 1.
     }
   };
 
