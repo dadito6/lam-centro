16f97cd070e8f6533b10f6cc2061ec666f8123a3
cesar ariel rea <cesararielrea9@gmail.com>
2025-10-22 09:02:29 -0300

    fixsobrenosotros

--- pages/sobre-nosotros.tsx
+++ pages/sobre-nosotros.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import Head from 'next/head';
-import { SiteHeader } from "@/components/site-header";
+import SiteHeader  from "@/components/site-header";
 
 export default function SobreNosotros() {
   return (
